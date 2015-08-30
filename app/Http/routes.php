2245a4baffcd0287e5430b5e2bb3682deb98dc5b
pikarin <aditia.pikarin.com>
2015-08-30 09:37:44 +0700

    Menambahkan area admin dan sistem autentikasi admin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,21 @@ Route::get('/', function () {
 
 get('blog', 'BlogController@index');
 get('blog/{slug}', 'BlogController@showPost');
+
+// Admin area
+get('admin', function () {
+  return redirect('/admin/post');
+});
+$router->group([
+  'namespace' => 'Admin',
+  'middleware' => 'auth',
+], function () {
+  resource('admin/post', 'PostController');
+  resource('admin/tag', 'TagController');
+  get('admin/upload', 'UploadController@index');
+});
+
+// Logging in and out
+get('/auth/login', 'Auth\AuthController@getLogin');
+post('/auth/login', 'Auth\AuthController@postLogin');
+get('/auth/logout', 'Auth\AuthController@getLogout');
